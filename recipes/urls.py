17381f0367370786ef4a3adb2c5f0295c8a6a25d
Mauricio Rosa <mauricioerwork@gmail.com>
2023-08-27 22:28:11 -0300

    Adicionando font-awesome

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from recipes.views import home
+from recipes.views import home, contato
 
 
 urlpatterns = [    
-    path('', home),    
+    path('', home),
+    path('contato/', contato)       
     
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html',context = {
+    return render(request, 'recipes/home.html',context = {
         "name": 'Mauiricio Rosa'
     })
 
-
+def contato(request):
+    return render(request, 'recipes/contato.html',context= {
+        "name": "Mauricio"
+    })
